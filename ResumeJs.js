09fec9260195ca7b87d409e55189f6406b6e139b
ajinxs <leeajink@gmail.com>
2017-12-28 12:52:28 +0530

    Added a project image and javascript funtionality to it to change the image to text

--- ResumeJs.js
+++ ResumeJs.js
@@ -4,3 +4,14 @@ function Mail(){
     
     document.getElementById('gmail').innerHTML='leeajink@gmail.com';    
 }
+
+//Project will show the description of the AppWatch Project
+function Project(){
+    document.getElementById('iOSApp').innerHTML = '<h3>Inventory Checking iOS (Watch) App Project</h3>The team had a task to develop application on Watch (iOS). The client vision was able to check items status,check inventory and add items to the inventory. The team was made of 6 individuals out which 4 were developers and 2 were business analysts. There was already some code available on which the team build and did the testing before launching. The resources was provided by the Thoughts Work. The tools and technology stack used were Javascript, CSS, Git. The project assessment took place on weekly contribution and meeting';
+}
+
+//Project will show the description of the iSO final year Project
+function ProjectPro()
+{
+    document.getElementById('iOSPro').innerHTML = '<h3>The Colony (iOS App Game)</h3>Objective to redesign the game and setup up the wifi infrastructure in order to play the game. The project consist of three parts: iOS, networking, UI design. The client vision was to create a game which will connect to the totems thru wifi and the player will able to interact with the totems by playing the available games. There was already a small prototype built on which the team did the testing before launching. Additional resources the team required was access to site where the team had to launch the final version of the product, some specific software used by the program and the previous code. The resources was provided by the client. The tools and technologies used were Swift, Java, Git, Trello and Slack. The project assessment took place on weekly contribution sheet, peer assessment and meeting minutes'
+}
